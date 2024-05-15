b5d266771899196409a4beca5dd4ba8853da0b46
Shubham Dhatwalia <shubhamdhatwalia1@gmail.com>
2024-05-15 13:09:47 +0530

    practice

--- src/app.js
+++ src/app.js
@@ -1,16 +1,73 @@
 let favmusic = document.querySelector(".fav-music-btn");
 let musicList = document.querySelector(".music-list");
 
-let removebtn = document.querySelector(".remove");
+let removebtn = document.querySelector(".close");
 
 favmusic.addEventListener("click", ()=>{
-    console.log("btn clicked");
-    console.log(musicList.classList);
     musicList.classList.toggle("show");
 });
 
 removebtn.addEventListener("click", ()=>{
-
-    console.log("btn clicked");
     favmusic.click();
+});
+
+
+
+
+let musicImage  =document.querySelector(".img-area img");
+let musicName = document.querySelector(".song-details .song-name");
+let playPauseBtn = document.querySelector(".play-pause");
+let mainAudio = document.querySelector("#main-audio");
+let container = document.querySelector(".container");
+let playPauseBtnText = document.querySelector(".play-pause i");
+console.log(mainAudio.src);
+
+
+
+
+//   ------------ Music Index ----------------------------
+let musicIndex = Math.floor((Math.random()*allMusic.length)+1);
+console.log(musicIndex);
+
+window.addEventListener("load", ()=>{
+    loadMusic(musicIndex);
+});
+
+
+//    -------------------------- load music function ------------
+function loadMusic(indexNumber){
+
+    musicImage.src = `./images/${allMusic[indexNumber-1].img}.jpg`;
+    mainAudio.src = `./songs/${allMusic[indexNumber-1].src}.mp3`;
+    musicName.innerText = `${allMusic[indexNumber-1].name}||${allMusic[indexNumber-1].artist}`;
+    console.log(mainAudio.src);
+}
+
+
+//  ----------------------- Play Music ------------------------------
+function playMusic(){
+    container.classList.add("paused");
+    playPauseBtnText.innerText = "pause";
+    mainAudio.play();
+};
+//      ------------------------ Pause Music ------------------------
+function pauseMusic(){
+    container.classList.remove("paused");
+    playPauseBtnText.innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+//      -------------------------- Play Pause Button --------------------
+
+playPauseBtn.addEventListener("click", ()=>{
+    const isPaused = container.classList.contains("paused");
+    isPaused ? pauseMusic():playMusic();
+});
+
+//   --------------------------  Add to favorite list ------------------------
+
+let addFavorite = document.querySelector(".add-to-favorite");
+
+addFavorite.addEventListener("click", ()=>{
+
 });
